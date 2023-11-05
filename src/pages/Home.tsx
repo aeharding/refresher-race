31aa3199df942732a40e754eb4104de6d82ca925
Alexander Harding <2166114+aeharding@users.noreply.github.com>
2023-11-05 17:52:12 -0600

    Add example

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
-import MessageListItem from '../components/MessageListItem';
-import { useState } from 'react';
-import { Message, getMessages } from '../data/messages';
+import MessageListItem from "../components/MessageListItem";
+import { useEffect, useState } from "react";
+import { Message, getMessages } from "../data/messages";
 import {
   IonContent,
   IonHeader,
@@ -8,27 +8,15 @@ import {
   IonPage,
   IonRefresher,
   IonRefresherContent,
+  IonSpinner,
   IonTitle,
   IonToolbar,
-  useIonViewWillEnter
-} from '@ionic/react';
-import './Home.css';
+  useIonViewWillEnter,
+} from "@ionic/react";
+import "./Home.css";
+import { VList } from "virtua";
 
 const Home: React.FC = () => {
-
-  const [messages, setMessages] = useState<Message[]>([]);
-
-  useIonViewWillEnter(() => {
-    const msgs = getMessages();
-    setMessages(msgs);
-  });
-
-  const refresh = (e: CustomEvent) => {
-    setTimeout(() => {
-      e.detail.complete();
-    }, 3000);
-  };
-
   return (
     <IonPage id="home-page">
       <IonHeader>
@@ -36,25 +24,55 @@ const Home: React.FC = () => {
           <IonTitle>Inbox</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonRefresher slot="fixed" onIonRefresh={refresh}>
-          <IonRefresherContent></IonRefresherContent>
-        </IonRefresher>
-
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">
-              Inbox
-            </IonTitle>
-          </IonToolbar>
-        </IonHeader>
-
-        <IonList>
-          {messages.map(m => <MessageListItem key={m.id} message={m} />)}
-        </IonList>
+      <IonContent>
+        <List />
       </IonContent>
     </IonPage>
   );
 };
 
+function List() {
+  const [messages, setMessages] = useState<Message[]>(getMessages());
+  const [loaded, setLoaded] = useState<boolean | undefined>(undefined);
+
+  const refresh = (e: CustomEvent) => {
+    setTimeout(() => {
+      e.detail.complete();
+    }, 1_000);
+  };
+
+  useEffect(() => {
+    setLoaded(false);
+
+    setTimeout(() => {
+      setLoaded(true);
+    }, 2000);
+  }, []);
+
+  const feed =
+    loaded && messages ? (
+      <VList
+        className="ion-content-scroll-host"
+        style={{ overflowY: "scroll" }}
+      >
+        {messages.map((m) => (
+          <MessageListItem key={m.id} message={m} />
+        ))}
+      </VList>
+    ) : (
+      ""
+    );
+
+  if (loaded === false) return <IonSpinner />;
+
+  return (
+    <>
+      <IonRefresher slot="fixed" disabled={false} onIonRefresh={refresh}>
+        <IonRefresherContent></IonRefresherContent>
+      </IonRefresher>
+
+      {feed}
+    </>
+  );
+}
 export default Home;
